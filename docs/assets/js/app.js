d73888c0e866414f841610154d3cfab126ad660f
Vladislav Pavlovskiy <pavlovskiy2799@gmail.com>
2020-05-29 00:08:14 +0300

    Сделал слайдер секции отзывов

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,14 +1,37 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+$(document).ready(function () {
+    $(".header__text .owl-carousel").owlCarousel({
         items: 1,
         loop: true,
         autoplay: true,
         autoplayTimeout: 5000,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             }
         },
-        
+
     });
-  });
+});
+
+// $(document).ready(function () {
+//     $(".reviews__block .owl-carousel").owlCarousel({
+//         items: 1,
+//         loop: true,
+//         dots: false,
+//     });
+// });
+
+$(document).ready(function () {
+    $('.reviews__block .owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        // autoplay: true,
+        startPosition: 2,
+        nav: true,
+        dotsContainer: '.reviews__block-nav .reviews__thumbs',
+    })
+});
+$('.reviews__thumbs-item').click(function () {
+    $('.reviews__block .owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+})
